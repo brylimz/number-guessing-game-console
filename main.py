8778800c859320b5757124926b81bb63f13f3e08
Bryan`Y. Limz <superuser@brylimz.com>
2021-11-30 09:49:20 +0700

    return added

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ HARD_LEVEL_TURNS = 5
 
 
 def set_difficulty():
-    level = input("Choose a difficulty. Type 'easy' or hard:")
+    level = input("Choose a difficulty. Type 'easy' or 'hard':")
     if level == "easy":
         return EASY_LEVEL_TURNS
     else:
@@ -16,6 +16,7 @@ def set_difficulty():
 
 
 def check_answer(guess, answer, turns):
+    """ checks answer against guess. Returns the number of turns remaining"""
     if guess > answer:
         print("Too high")
         return turns - 1
@@ -34,13 +35,17 @@ def game():
     print(f"The answer is {answer}")
 
     turns = set_difficulty()
-    print(f"You have {turns} attempts remaining to guess the number")
 
     # repeat the guessing functionality if they get it wrong.
     guess = 0
     while guess is not answer:
+        print(f"You have {turns} attempts remaining to guess the number")
+
         guess = int(input("Make a guess: "))
-        check_answer(guess, answer, turns)
+        turns = check_answer(guess, answer, turns)
+        if turns == 0:
+            print("You've run of guesses you lose")
+            return
 
 # track the number of turns and reduce by 1 if they get it wrong.
 
