bfc58ac32552f4d67802d64e17310fea48b6a5de
Bryan`Y. Limz <superuser@brylimz.com>
2021-11-30 09:10:21 +0700

    logic started

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
+from random import randint
+
+# function to check user's guess against actual answer.
+
+
+def check_answer(guess, answer):
+    if guess > answer:
+        print("Too high")
+    elif guess < answer:
+        print("Too low.")
+    else:
+        print(f"You got it! the answer is {answer}")
+
+
 # choosing a random number between 1 and 100.
+print("Welcome to the Number Guessing Game !")
+print("I'm thinking of a number between 1 and 100.")
+answer = randint(1, 100)
 
 # make function to set difficulty.
 
-# let the user guess a number.
 
-# function to check user's guess against actual answer.
+def set_difficulty():
+    input("Choose a difficulty")
+
+# let the user guess a number.
+guess = int(input("Make a guess:"))
+
 
 # track the number of turns and reduce by 1 if they get it wrong.
 
 # repeat the guessing functionality if they get it wrong.
-print("Welcome to the Number Guessing Game !")
+
 
