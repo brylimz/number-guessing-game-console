3b7a3b67be5007576310115296c9fdda3af93301
Bryan`Y. Limz <superuser@brylimz.com>
2021-11-30 09:37:52 +0700

    fixing bug

--- main.py
+++ main.py
@@ -12,37 +12,37 @@ def set_difficulty():
         return HARD_LEVEL_TURNS
 
 
-
 # function to check user's guess against actual answer.
 
 
-def check_answer(guess, answer):
+def check_answer(guess, answer, turns):
     if guess > answer:
         print("Too high")
+        return turns - 1
     elif guess < answer:
         print("Too low.")
+        return turns - 1
     else:
         print(f"You got it! the answer is {answer}")
 
 
 # choosing a random number between 1 and 100.
-print("Welcome to the Number Guessing Game !")
-print("I'm thinking of a number between 1 and 100.")
-answer = randint(1, 100)
-print(f"The answer is {answer}")
+def game():
+    print("Welcome to the Number Guessing Game !")
+    print("I'm thinking of a number between 1 and 100.")
+    answer = randint(1, 100)
+    print(f"The answer is {answer}")
 
-turns = set_difficulty()
-print(f"You have {turns} attempts remaining to guess the number")
+    turns = set_difficulty()
+    print(f"You have {turns} attempts remaining to guess the number")
 
-
-
-# make function to set difficulty.
-
-
-
-# let the user guess a number.
-guess = int(input("Make a guess: "))
+    # repeat the guessing functionality if they get it wrong.
+    guess = 0
+    while guess is not answer:
+        guess = int(input("Make a guess: "))
+        check_answer(guess, answer, turns)
 
 # track the number of turns and reduce by 1 if they get it wrong.
 
-# repeat the guessing functionality if they get it wrong.
+
+game()
